176d78310b5598242a2ae3f79ee20c8dbde88b42
2333xiang <odokoei@outlook.com>
2024-11-16 17:37:45 +0800

    添加并行处理线程池

---
+++ src/parallel/CoreUser.h
@@ -0,0 +1,27 @@
+#ifndef COREUSER_H
+#define COREUSER_H
+
+#include <condition_variable>
+#include <mutex>
+#include <thread>
+
+class ParallelWorker {
+  // 总线程数与完成工作的线程数
+  int _threadcount;
+  int _completedthreadcount;
+  // 所有的线程
+  std::thread *threads;
+  // 线程同步变量
+  std::mutex mtx;
+  std::condition_variable cv;
+
+  void *(*task)();
+
+public:
+  // 构造ParallelWorker
+  ParallelWorker(int threadcount);
+  // 析构ParallelWorker
+  virtual ~ParallelWorker();
+};
+
+#endif /* COREUSER_H */
